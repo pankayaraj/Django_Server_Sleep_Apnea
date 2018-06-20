2b3b5cafe1368478372cb185a7c15cac8bee2abf
punk <pankayaraj1995@gmail.com>
2018-06-20 18:32:13 +0530

    Revert "Inital_commit"
    This reverts commit 4aba7f7dd888ae2fbad3f50ac26ca62105d4903d.

--- SERVER/clients/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Client_buffer_recv, Devices, Patient
-# Register your models here.
-
-
-
-admin.site.register(Client_buffer_recv)
-admin.site.register(Devices)
-admin.site.register(Patient)
--- SERVER/clients/static/my/my.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-    $("#page-wrapper").load("dashboard.html"); 
-});
-
-function myJsFunc(x){
-    $("#page-wrapper").load(x+".html"); 
-}
